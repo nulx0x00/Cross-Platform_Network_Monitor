828fdb44a2452a25be7d1c187af8af64833dc66c
Nulx0x00 <vanshnavyavn@gmail.com>
2026-03-14 20:02:15 +0530

    added my project

---
+++ main.py
@@ -0,0 +1,23 @@
+import subprocess
+import platform
+
+# target = ["8.8.8.8", "127.0.0.1","1.1.1.1"]
+
+if platform.system().lower() == "windows":
+    flag = "-n"
+else:
+    flag = "-c"
+
+with open("target.txt", "r") as file:
+    for line in file:
+        try:
+            command = f"ping {flag} 1  {line.strip()} "
+            print(f"checking......  {line.strip()}")
+            result = subprocess.run(command, shell = True, capture_output=True, text = True, timeout=2 )
+            if result.returncode == 0:
+                print(f" [+] {line.strip()} is up ")
+            else :
+                print (f" [-] {line.strip()} is down")
+                print(f"DEBUG: {result.stderr}")
+        except subprocess.TimeoutExpired:
+            print(f" [-] {line.strip()} is down (Timed Out)")
